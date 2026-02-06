3083e20efbc01c0018f41606ee8cf3c4250c70ca
Mahadeva Prasad M <mahadevaprasadcs23@gmail.com>
2026-02-06 23:10:22 +0530

    Add selection sort implementation in sort.py
    Implemented selection sort algorithm to sort an array in ascending order.

--- Bruteforce-Approaches/Sorting/Selection Sort/sort.py
+++ Bruteforce-Approaches/Sorting/Selection Sort/sort.py
@@ -1,1 +1,12 @@
+# Selection Sort for ascending order
 
+arr = [5, 3, 15, 0]
+for i in range(len(arr)):
+    min_index = i
+    for j in range(i+1, len(arr)):
+        if arr[min_index] > arr[j]:
+            min_index = j
+    if i != min_index:
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+print(arr)
